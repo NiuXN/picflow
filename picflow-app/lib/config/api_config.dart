712f3fd2d9880e468ded6391401492d2101020e2
niulonglong <17600313417>
2026-05-30 18:27:53 +0800

    fix: Android模拟器自动使用10.0.2.2连接后端

--- picflow-app/lib/config/api_config.dart
+++ picflow-app/lib/config/api_config.dart
@@ -1,9 +1,14 @@
-import 'package:flutter/foundation.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb, kReleaseMode;
 
 class ApiConfig {
   ApiConfig._();
 
-  static const String _devBaseUrl = 'http://localhost:8080/v1';
+  static String get _devBaseUrl {
+    if (kIsWeb) return 'http://localhost:8080/v1';
+    if (Platform.isAndroid) return 'http://10.0.2.2:8080/v1';
+    return 'http://localhost:8080/v1';
+  }
   static const String _prodBaseUrl = 'https://api.picflow.app/v1';
 
   static String get baseUrl => kReleaseMode ? _prodBaseUrl : _devBaseUrl;
